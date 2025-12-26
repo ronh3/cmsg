959fcc812517a84e7847a97a3ee836031af04d23
Ron <rholcomb3@gmail.com>
2025-12-26 02:00:52 -0800

    Reorganized structure to include first, second, and third person messages. Modified cmsg function to delete the actual line by default. Added in first person blademaster lines. Still missing a few, primarly infusions and a few shindo items.

--- src/scripts/Combat msgs brief/Combat_msgs.lua
+++ src/scripts/Combat msgs brief/Combat_msgs.lua
@@ -1,5 +1,9 @@
+cmsg = cmsg or {}
+cmsg.replaceLines = cmsg.replaceLines or true
+
 function cmsg(who, what, victim, ...)
   if ... then affs = {...} end
+  if cmsg.replaceLines then deleteLine() end
   cecho(f"\n<green>{who:title()}<reset>: <green>{what:title()}<reset>")
   if victim then cecho(f" -\> <red>{victim:title()}") end
   if affs then
